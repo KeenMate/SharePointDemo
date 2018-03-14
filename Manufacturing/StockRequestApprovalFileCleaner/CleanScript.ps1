d1fb5c3c22fde230a0aef06b2cdcb2254d9c51c2
jakubsenk <jakub.senk@keenmate.com>
2018-03-14 17:50:14 +0100

    PowerShell Variant

---
+++ Manufacturing/StockRequestApprovalFileCleaner/CleanScript.ps1
@@ -0,0 +1,4 @@
+﻿$path = "C:\Git\SharePointDemo\Manufacturing\StockRequestApprovalFileCleaner\FileCleaner\FileCleaner\bin\Debug\this";
+$time = (Get-Date).AddDays(-1);
+
+Get-ChildItem $path | Where-Object {$_.CreationTime -lt $time} | Remove-Item;
